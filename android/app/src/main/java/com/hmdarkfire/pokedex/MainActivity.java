aadd59ed3ffa3916388418f24b03e473247e75f6
Henrique Marques <henrique_marques.dev@outlook.com>
2022-07-31 15:54:20 -0300

    feat: add splash, icon and adaptive-icon

--- android/app/src/main/java/com/hmdarkfire/pokedex/MainActivity.java
+++ android/app/src/main/java/com/hmdarkfire/pokedex/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pokedex;
+package com.hmdarkfire.pokedex;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/hmdarkfire/pokedex/MainApplication.java
+++ android/app/src/main/java/com/hmdarkfire/pokedex/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pokedex;
+package com.hmdarkfire.pokedex;
 
 import android.app.Application;
 import android.content.Context;
@@ -85,7 +85,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.pokedex.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.hmdarkfire.pokedex.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
